ef5354f1567e47c34e0134345f25674785e80892
r0227n <developryo@gmail.com>
2024-09-22 14:26:10 +0900

    add: Repository 詳細情報得 method

--- lib/repository/github/github_repo_repository.dart
+++ lib/repository/github/github_repo_repository.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'github_client.dart';
 import 'github_repository.dart';
 import 'models/search_response.dart';
+import 'models/repository.dart';
 
 /// GitHub Search API's
 class GithubRepoRepository extends GithubRepository {
@@ -48,5 +49,25 @@ class GithubRepoRepository extends GithubRepository {
     );
 
     return SearchResponse.fromJson(jsonDecode(response.body), type: SearchEntity.repositories);
+  }
+
+  /// Get a repository
+  ///
+  /// doc: https://docs.github.com/ja/rest/repos/repos?apiVersion=2022-11-28#get-a-repository
+  Future<Repository> get(
+    String fullName, {
+    String? token,
+    String? apiVersion,
+  }) async {
+    const apiType = 'repos';
+
+    final response = await GithubClient.request(
+      token: token ?? this.token,
+      url: Uri.http(GithubRepository.host, '$apiType/$fullName'),
+      method: HttpMethod.get,
+      apiVersion: apiVersion ?? this.apiVersion,
+    );
+
+    return Repository.fromJson(jsonDecode(response.body));
   }
 }
